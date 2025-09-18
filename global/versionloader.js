e24fa2fddb29692038d8f8d33536e85de5b70ea5
Pxsl <151259803+PxslGames@users.noreply.github.com>
2025-09-18 13:10:34 +0100

    Delete global/versionloader.js

--- global/versionloader.js
+++
@@ -1,9 +0,0 @@
-fetch('/version.json')
-  .then(res => res.json())
-  .then(data => {
-    const footer = document.getElementById('site-footer');
-    footer.textContent = `pxsl.dev - v${data.version} - made by pxsl`;
-  })
-  .catch(err => {
-    console.error("Failed to load version.json:", err);
-  });
